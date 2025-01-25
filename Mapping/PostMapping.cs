3c5a2fa3ed337c6a7a834f4bff61d4aea849205c
BAHMAN <imbahmanranjbar@gmail.com>
2025-01-25 21:16:36 +0330

    ADD Mapping Models

---
+++ Mapping/PostMapping.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PostManagementSystem.Models;
+
+namespace PostManagementSystem.Mapping
+{
+    public class PostMapping : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Posts");
+            builder.HasKey(p => p.ID);
+
+            builder.Property(p => p.urlImage).IsRequired()
+              .HasAnnotation("eror", "the image is required");
+            builder.Property(p => p.Description)
+                .IsRequired()
+              .HasAnnotation("eror", "the des is required");
+            builder.Property(p => p.IsDeleted)
+                .IsRequired();
+
+            builder.HasOne(p => p.User)
+                .WithMany(p => p.Post)
+                .HasForeignKey(p => p.UserID);
+
+        }
+    }
+}
---
+++ Mapping/UserMapping.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PostManagementSystem.Models;
+
+namespace PostManagementSystem.Mapping
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(p => p.ID)
+                ;
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasAnnotation("Eror", "the Name is Required");
+            builder.Property(p => p.Email)
+                .IsRequired()
+                .HasAnnotation("Eror", "the Email is Required");
+            builder.HasIndex(p => p.UserName)
+                .IsUnique()
+                .HasAnnotation("Eror", "the UserName is Required and must be Unique");
+            builder.Property(p => p.Password)
+              .IsRequired()
+              .HasAnnotation("Eror", "the Password is Required");
+            builder.Property(p => p.IsDeleted);
+
+            builder.HasMany(c => c.Post)
+               .WithOne(p => p.User)
+               .HasForeignKey(t => t.UserID);
+        }
+    }
+}
